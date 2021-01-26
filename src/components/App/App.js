e1d02c40bf8ca2ef8d170599b5a6bce6916346b1
ibasovskyy <igor.basovskyy@engineering.ait.com>
2021-01-26 21:09:51 +0200

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Spinner from './components/Spinner/Spinner';
-import Error from './components/Error/Error';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Spinner from '../Spinner/Spinner';
+import Error from '../Error/Error';
 
-import weatherApi from '../src/apis';
+import weatherApi from '../../apis';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
